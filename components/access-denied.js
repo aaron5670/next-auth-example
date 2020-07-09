ae8bf6820705cc912431d93b594c1143426ac4b3
aaron5670 <a.vdberg98@gmail.com>
2020-07-09 21:23:12 +0200

    Translated text

--- components/access-denied.js
+++ components/access-denied.js
@@ -2,7 +2,7 @@ import Layout from '../components/layout'
 
 export default () => (
   <Layout>
-    <h1>Access Denied</h1>
+    <h1>Toegang geweigerd</h1>
     <p>
       <a href="/api/auth/signin">Je moet ingelogd zijn om deze pagina te kunnen bekijken.</a>
     </p>
